ec91ca4fe4c163f129b69ac9202a6a257cd0fff9
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-21 10:29:00 +1100

    adds project form and logic to add task and projects in storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import {storageManager} from "./localStorage";
-let projects=storageManager.initStorage();
+import { storageManager } from "./localStorage";
+let projects = storageManager.downloadProjects();
+
 let content = document.getElementById("content");
 
 const taskManager = (function () {
-
-   
-    let tasks=[];
-    //ALl about creating a task
-    let createTask = function () {  
+    //all about creating a task
+    let createTask = function () {
 
         //class to create a task object
         class Task {
@@ -25,23 +23,30 @@ const taskManager = (function () {
         let taskDate = document.getElementById("task-date").value;
         let taskDescription = document.getElementById("task-description").value;
         let taskPriority = document.getElementById("task-priority").value;
-        //inserire la funziona che prenda la task e lo assegni al progetto. se non viene selezionata la casella il progetto è default
-        
-        //create the task object
-        let newTask = new Task(taskTitle, taskDate, taskDescription, taskPriority);
-        tasks.push(newTask);
-        //if doesn thave project, task assigned to default project
-        if(newTask.taskProject==undefined){
-            newTask.taskProject="default"
+        let taskProject = document.getElementById("select-project-menu");
+        //Check if there s the menu to select a project
+        if (taskProject) {
+            taskProject = taskProject.value;
+            let newTask = new Task(taskTitle, taskDate, taskDescription, taskPriority, taskProject);
+            //Put the created task in the belonging project
+            projects.forEach(obj => {
+                if (obj.projectName == newTask.taskProject) {
+                    obj.tasks.push(newTask);
+                    storageManager.saveProject(projects);//save the updated project in local storage
+                }
+            })
+        } else {
+            
+            taskProject = "default";
+            let newTask = new Task(taskTitle, taskDate, taskDescription, taskPriority, taskProject);
+            //the new task is been putted in the default project
+            let defaultProject = projects.find(obj => obj.projectName == "default");
+            defaultProject.tasks.push(newTask);
+            storageManager.saveProject(projects);
+
         }
-        // add the task to the belonging project
-        projects.forEach(obj=>{
-            if(obj.projectName==newTask.taskProject){
-                obj.tasks=tasks;
-                storageManager.saveProject();//save the updated project in local storage
-            }
-        })
-        content.innerHTML="";
+
+        content.innerHTML = "";
     }
 
     let createTaskForm = function () {
@@ -61,28 +66,78 @@ const taskManager = (function () {
             <option value="High">High</option>
         </select>
         <button type="button" id="create-task-btn">Create Task</button>
-    </form>`
+    </form>`;
+        //give the option to choose in whic project the task will be putted
+        if (projects.length == 1) {
+            document.getElementById("checkbox").style.display = "none";
+            document.querySelector(`label[for="checkbox"]`).style.display = "none";
+        } else {
+            let checkbox = document.getElementById("checkbox");
+            checkbox.addEventListener("change", function () {
+                if (checkbox.checked) {
+                    let selectProject = document.createElement("select");
+                    projects.forEach(obj => {
+
+                        selectProject.id = "select-project-menu";
+                        let projectOption = document.createElement("option");
+                        projectOption.setAttribute("value", obj.projectName);
+                        projectOption.textContent = obj.projectName;
+                        selectProject.appendChild(projectOption);
+                    })
+                    checkbox.insertAdjacentElement("afterend", selectProject);
+                }
+
+            })
+        }
+        content.innerHTML = "";
     }
-    let getTasks=function(){
-        return tasks;
-    }
+
     return {
         createTask,
-        createTaskForm,
-        getTasks
+        createTaskForm
     }
 
 
 })();
 
-const projectManager=(function(){
+const projectManager = (function () {
     class Project {
         constructor(projectName, date, priority) {
             this.projectName = projectName;
-            this.date = date;  
+            this.date = date;
             this.priority = priority;
+            this.tasks = [];
         }
     }
+    let createProjectForm = function () {
+        content.innerHTML = `<form action="" method="" id="project-form">
+        <label for="project-name">Project Name:</label>
+        <input type="text" id="project-name" name="project-name">
+        <label for="project-date">Due date:</label>
+        <input type="date" id="project-date" name="project-date">
+        <label for="project-priority">Priority:</label>
+        <select name="project-priority" id="project-priority">
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+        </select>
+        <button type="button" id="create-project-btn">Create Project</button>
+    </form>`
+    }
 
+    let createProject = function () {
+        let projectName = document.getElementById("project-name").value;
+        let projectDate = document.getElementById("project-date").value;
+        let projectPriority = document.getElementById("project-priority").value;
+        let newProject = new Project(projectName, projectDate, projectPriority);
+        projects = storageManager.downloadProjects();
+        projects.push(newProject);
+        storageManager.saveProject(projects);
+    }
+
+    return {
+        createProjectForm,
+        createProject
+    }
 })();
-export {taskManager};
+export { taskManager, projectManager };
