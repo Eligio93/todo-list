3385190974ea468418746659ced9cd173bc71c3c
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-21 10:27:52 +1100

    adds UI for creating project and task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Ciao");
+import "./style.css";
+import createSideBar from "./createSideBar";
+import {taskManager,projectManager} from "./App";
+let content=document.getElementById("content");
+
+
+let addTask=function(){
+    taskManager.createTaskForm();
+    let createTaskBtn=document.getElementById("create-task-btn");
+    createTaskBtn.addEventListener("click",function(){
+        taskManager.createTask();
+    });  
+}
+let addProject=function(){
+    projectManager.createProjectForm();
+    let createProjectBtn=document.getElementById("create-project-btn");
+    createProjectBtn.addEventListener("click",function(){
+        projectManager.createProject();
+    })
+}
+
+createSideBar();
+
+let createTodo=document.getElementById("create-todo");
+createTodo.addEventListener("click",function(){
+    content.innerHTML="";
+    let newTaskBtn=document.createElement("button");
+    newTaskBtn.id="new-task-btn";
+    newTaskBtn.textContent="New Task";
+    content.appendChild(newTaskBtn);
+    let newProjectBtn=document.createElement("button");
+    newProjectBtn.id="new-project-btn";
+    newProjectBtn.textContent="New Project";
+    content.appendChild(newProjectBtn);
+    newTaskBtn.addEventListener("click",addTask);
+    newProjectBtn.addEventListener("click",addProject)
+
+});
