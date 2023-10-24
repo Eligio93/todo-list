f47ecc098ad8d387f2c53722b363a2c3fb2deb78
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-24 23:34:26 +1100

    add check validity to task and projects form

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import createSideBar from "./createSideBar";
 import {taskManager,projectManager} from "./App";
 import {displayController} from "./UI";
 let content=document.getElementById("content");
+let sideBar=document.getElementById("sidebar")
 
 
 let addTask=function(){
     taskManager.createTaskForm();
     let createTaskBtn=document.getElementById("create-task-btn");
     let taskForm=document.getElementById("task-form");
-    taskForm.addEventListener("submit",function(event){
-        //prevent the submit event but checks the inout fields
-        event.preventDefault()
-        //work as a click event
-        createTaskBtn.click();
-        taskManager.createTask();
+    createTaskBtn.addEventListener("click",function(event){
+        //check validity of the form and date
+        if(taskForm.checkValidity()){
+            taskManager.createTask();  
+        }else if(new Date(document.getElementById("task-date").value)< new Date()){
+           alert("Your task can't have a date in the past")
+        }else{
+            alert("All informations requested are needed");
+        }
+        
     });  
 }
 
@@ -22,11 +27,23 @@ let addProject=function(){
     projectManager.createProjectForm();
     let createProjectBtn=document.getElementById("create-project-btn");
     createProjectBtn.addEventListener("click",function(){
-        projectManager.createProject();
+        let projectForm=document.getElementById("project-form");
+        if(projectForm.checkValidity()){
+            projectManager.createProject();    
+        }else if(new Date(document.getElementById("project-date").value)< new Date()){
+            alert("Your project date can't have a date in the past")
+         }else{
+             alert("All informations requested are needed");
+         }
+        sideBar.innerHTML="";
+        createSideBar();
+        
     })
 }
 
+
 createSideBar();
+displayController.homeTasks();
 
 let createTodo=document.getElementById("create-todo");
 createTodo.addEventListener("click",function(){
@@ -46,16 +63,10 @@ createTodo.addEventListener("click",function(){
 
 let homeBtn=document.getElementById("home-btn");
 homeBtn.addEventListener("click",function(){
-    displayController.homeTasks();
-    let descriptionBtns=document.querySelectorAll(".arrow-down-icon");
-    descriptionBtns.forEach((btn,index)=>{
-        btn.addEventListener("click",function(){
-            let description=document.querySelector(`[data-description="${index}"]`);
-            if(description.style.display=="none"){
-                description.style.display="block";
-            }else if(description.style.display=="block"){
-                description.style.display="none";
-            }
-        })
-    })
+    displayController.homeTasks();  
 });
+let todayBtn=document.getElementById("today-btn");
+todayBtn.addEventListener("click",displayController.todayTasks);
+
+let weekBtn=document.getElementById("this week-btn");
+weekBtn.addEventListener("click",displayController.weekTasks);
